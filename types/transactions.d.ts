0541e9fb1501f3ce79a8dd12a62f2f3a7894bd03
Malik <62953363+maliksvd@users.noreply.github.com>
2024-10-20 20:52:32 -0400

    refactor(types): update Transaction interface with nullable fields and add amount

--- types/transactions.d.ts
+++ types/transactions.d.ts
@@ -1,9 +1,10 @@
-declare interface Transaction {
+export interface Transaction {
   id: number
-  user_id: string
+  user_id: string | null
   transaction_date: string
-  type: string
-  description: string
-  category: string
-  account: string
+  type: string | null
+  description: string | null
+  category: string | null
+  account: string | null
+  amount: number | null
 }
