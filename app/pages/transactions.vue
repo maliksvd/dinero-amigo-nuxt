72e5ef2b13cca9667642e08ba7c3886338e86fe1
Malik <62953363+maliksvd@users.noreply.github.com>
2024-10-20 20:51:35 -0400

    fix(transaction): correct component name for transaction list

--- app/pages/transactions.vue
+++ app/pages/transactions.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
+  import type { Database } from '~~/types/database.types'
+
   const client = useSupabaseClient<Database>()
   const user = useSupabaseUser()
 </script>
 
 <template>
   <div>
-    <h1>Transactions</h1>
-    <TransactionList />
+    <TransactionsList />
   </div>
 </template>
